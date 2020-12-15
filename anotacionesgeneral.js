56b5e381db23e4d7543839b9be7a7802602715db
Diego Ortiz Gamboa <diegofergamboa@hotmail.es>
2020-12-15 17:04:29 -0500

    Se conocio como unir al codigo un script de javascript

--- anotacionesgeneral.js
+++
@@ -1,9 +0,0 @@
-/* En Javascript se pueden usar muchas maneras y muchos programas, el objetivo es justamente de este
-    proyecto es realizar reto y aplicaciones para empezar a usar javascript se usará el curso de fazt */
-
-/* Javascript siempre fue enfocado a los navegadores, es por esto que es muy importante saber que 
-    funciona siempre en los navegadores web, ya que es enfocado a desarrollo web */
-
-
-
-
---
+++ codigo.js
@@ -0,0 +1,3 @@
+string = "naranja oscurito"
+numero = 1
+booleano = false
