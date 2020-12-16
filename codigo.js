e0b60fe4359ae6e37e4f53cbe8d57c0810a99109
Diego Ortiz Gamboa <diegofergamboa@hotmail.es>
2020-12-15 20:26:26 -0500

    Se estudiaron operador logicos

--- codigo.js
+++ codigo.js
@@ -2,11 +2,25 @@
 
 function saludar(nombre_usuario) {
     nombre_usuario = prompt("¿Como es tu nombre: ?");
-    document.write(`Hola, ${nombre_usuario} bienvenido al curso general de HTML`);
+    document.write(`Hola, ${nombre_usuario} bienvenido al curso general de Javascript`);
 }
 
 
 
 saludar()
 
+// Aplicacion de los concat
+// Se usa para unir un string y un numero.
 
+
+
+// OPERADORES LOGICOS.
+// Sirven para realizar comparaciones entre dos variables y devuelve un valor Booleano
+
+let numero = 23;
+let numero2 = 5;
+
+// document.write(numero === numero2);
+
+// != Son diferentes
+// Pregunta si el valor es igual con ==, pero con === pregunta si es estrictamente igual
