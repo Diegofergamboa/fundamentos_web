4f519916c268f3d9a0274ec5d33d98772d55f813
Diego Ortiz Gamboa <diegofergamboa@hotmail.es>
2020-12-15 19:39:15 -0500

    Se ha agregado un reto interesante

---
+++ reto1/codigo.js
@@ -0,0 +1,15 @@
+alert("hola mundo");
+
+
+var a = prompt("Escribe el primer numero: ")
+var b = prompt("Escribe el segundo numero: ")
+
+function suma(a,b){
+    c = a + b;
+    return c;
+}
+
+alert(suma(a,b));
+
+console.log("Esto es un mensaje por consola")
+console.log(c);
